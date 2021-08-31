08e4e9ef77a3d72890035832703548463dd4dd34
ssssujini99 <sssssjin99@naver.com>
2021-08-31 12:40:32 +0000

    accounts/views.py 수정

--- accounts/views.py
+++ accounts/views.py
@@ -5,20 +5,20 @@ from .forms import SignupForm, LoginForm # 회원가입폼, 로그인폼
 
 # Create your views here.
 
-def signup(request):
+def signup(request): # 회원가입
     if request.method == 'POST':
         form = SignupForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
-            return redirect('accounts:login')
+            return redirect('accounts:login') # accounts의 login으로 redirect하기
         else:
-            form = SignupForm()
+            form = SignupForm() 
         return render(request, 'accounts/signup.html', {
             'form': form,
-        })
+        }) # 다시 회원가입을 요청
 
 
-def login_check(request):
+def login_check(request): # 로그인
     if request.method == "POST":
         form = LoginForm(request.POST)
         name = request.POST.get('username')
@@ -26,18 +26,18 @@ def login_check(request):
         
         user = authenticate(username=name, password=pwd)
         
-        if user is not None:
-            login(request, user)
+        if user is not None: # user가 데이터베이스에 있다면
+            login(request, user) # 장고가 제공하는 기능 login
             return redirect("/")
         else:
             return render(request, 'accounts/login_fail_info.html')
-    else: ## request.method == "GET"
-        form = LoginForm()
+    else: ## request.method != "GET"
+        form = LoginForm() # 다시 로그인 시도
         return render(request, 'accounts/login.html', {"form": form})
     
     
-def logout(request):
-    django_logout(request)
+def logout(request): # 로그아웃
+    django_logout(request) # 장고에서 제공
     return redirect("/")
         
     
